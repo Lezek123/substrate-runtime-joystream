fa596a2d59dd5afec003b143939d4d1c9c4d3ea6
Leszek Wiesner <l.wiesner@adawards.pl>
2020-05-29 10:13:47 +0200

    Let's fail the build

--- pioneer/packages/joy-proposals/src/Proposal/ChooseProposalType.tsx
+++ pioneer/packages/joy-proposals/src/Proposal/ChooseProposalType.tsx
@@ -9,14 +9,14 @@ import Loading from "./Loading";
 import "./ChooseProposalType.css";
 import { RouteComponentProps } from "react-router-dom";
 
-export const Categories = {
+export const Categories = false; /*{
   storage: "Storage",
   council: "Council",
   validators: "Validators",
   cwg: "Content Working Group",
   other: "Other"
 } as const;
-
+*/
 export type Category = typeof Categories[keyof typeof Categories];
 
 export default function ChooseProposalType(props: RouteComponentProps) {
