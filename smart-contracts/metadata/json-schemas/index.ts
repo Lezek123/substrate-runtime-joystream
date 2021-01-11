7294ba28b546ffbc429bcc2b4ee665a033aeb169
Leszek Wiesner <leszek@jsgenesis.com>
2021-01-11 10:08:41 +0100

    Channel/Video metadata protobufs + json schema / protobufs / tooling fixes

---
+++ smart-contracts/metadata/json-schemas/index.ts
@@ -0,0 +1,2 @@
+export { default as ChannelMetadataSchema } from './ChannelMetadata.json'
+export { default as VideoMetadataSchema } from './VideoMetadata.json'
---
+++ smart-contracts/metadata/types/index.ts
@@ -0,0 +1,2 @@
+export { ChannelMetadata } from './ChannelMetadata'
+export { VideoMetadata, VideoMedia, License, MediaLocation } from './VideoMetadata'
