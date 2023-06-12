6a6feac57cc31f7fbe779d2cf10db25f53fd5d57
Lezek123 <leszek@jsgenesis.com>
2023-06-12 11:12:21 +0200

    Test Orion integration tests on specific monorepo PR branch logic

--- tests/network-tests/src/scenarios/orion.ts
+++ tests/network-tests/src/scenarios/orion.ts
@@ -3,5 +3,6 @@ import buyingMemberships from '../flows/membership/buyingMemberships'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 scenario('Orion', async ({ job }) => {
+  console.log('Running integration tests on PR branch')
   job('buying memberships', buyingMemberships)
 })
